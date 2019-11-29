ddf72abf421f5ae297d7e10c2a9fbe5f65dfaa70
Liviu Cauia <liviu.cauia@gmail.com>
2019-11-29 16:59:00 +0100

    mister cocktail

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,33 @@
+class DosesController < ApplicationController
+  before_action :set_dose, only: [:show, :destroy]
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+
+    if @dose.save
+      redirect_to @cocktail
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  def set_dose
+    @dose = Dose.find(params[:id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts 'Cleaning database...'
+Dose.destroy_all
+
+puts 'Creating dose...'
+dose_attributes = [
+  {
+    description:         '20',
+    cocktail_id:           22,
+    ingredient_id:         1
+  },
+    {
+    description:         '30',
+    cocktail_id:           23,
+    ingredient_id:          1
+  },
+    {
+    description:         '40',
+    cocktail_id:           24,
+    ingredient_id:          1
+  }]
+Dose.create!(dose_attributes)
+puts 'Finished!'
+
+puts 'Creating ingredient...'
+ingredient_attributes = [
+  {
+    name:         'strawberry',
+  },
+    {
+     name:         'blueberry',
+  },
+    {
+     name:         'cranberry',
+  }]
+Ingredient.create!(ingredient_attributes)
+puts 'Finished!'
